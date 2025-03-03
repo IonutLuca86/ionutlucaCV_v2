aaa5948e6520d08c3d89493d9e947d1500c06925
Luca Ionut-Catalin <112466341+IonutLuca86@users.noreply.github.com>
2025-03-03 14:51:51 +0100

    email.js fix try

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  define: {
+    'import.meta.env.VITE_EMAILJS_SERVICE_ID': JSON.stringify(import.meta.env.VITE_EMAILJS_SERVICE_ID),
+    'import.meta.env.VITE_EMAILJS_TEMPLATE_ID': JSON.stringify(import.meta.env.VITE_EMAILJS_TEMPLATE_ID),
+    'import.meta.env.VITE_EMAILJS_PUBLIC_KEY': JSON.stringify(import.meta.env.VITE_EMAILJS_PUBLIC_KEY),
+  },  
 })
