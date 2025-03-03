a571337bbeeec52e8fb0c241993e0094319bcb8b
Luca Ionut-Catalin <112466341+IonutLuca86@users.noreply.github.com>
2025-03-03 14:54:04 +0100

    fixed deploy error

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  define: {
-    'import.meta.env.VITE_EMAILJS_SERVICE_ID': JSON.stringify(import.meta.env.VITE_EMAILJS_SERVICE_ID),
-    'import.meta.env.VITE_EMAILJS_TEMPLATE_ID': JSON.stringify(import.meta.env.VITE_EMAILJS_TEMPLATE_ID),
-    'import.meta.env.VITE_EMAILJS_PUBLIC_KEY': JSON.stringify(import.meta.env.VITE_EMAILJS_PUBLIC_KEY),
-  },  
+  
 })
